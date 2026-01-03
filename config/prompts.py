5548db0993ad2ba00c778bf6f46f40b508132f81
Dhruv Dhankher <156188075+dhruvd-1@users.noreply.github.com>
2026-01-03 15:13:19 +0530

    Create prompts.py

---
+++ config/prompts.py
@@ -0,0 +1,24 @@
+"""
+Prompt templates for all agents
+(We'll populate these in later steps)
+"""
+
+# Step 5: Drafter Agent
+DRAFTER_PROMPT = """
+# TODO: Will implement in Step 5
+"""
+
+# Step 6: Critic Agent
+CRITIC_PROMPT = """
+# TODO: Will implement in Step 6
+"""
+
+# Step 7: Guardian Agent
+GUARDIAN_PROMPT = """
+# TODO: Will implement in Step 7
+"""
+
+# Step 8: Pedagogy Agent
+PEDAGOGY_PROMPT = """
+# TODO: Will implement in Step 8
+"""
